6fd9b84506da3ad33334fe4bf1f44977c7e7cf3b
JPHA <josepabloha@live.com.mx>
2025-10-10 20:29:22 -0600

    api on render

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://api-login-uqgo.onrender.com';
 
 // Crear instancia de axios
 const apiClient = axios.create({
